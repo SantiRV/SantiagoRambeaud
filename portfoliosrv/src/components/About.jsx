06802e85b5bfbad297c5ae42329aee9eccddbe24
Santiago Rambeaud Vazquez <santirambeaudvazquez@gmail.com>
2024-12-10 17:35:06 +0100

    cambios en los estilos

--- portfoliosrv/src/components/About.jsx
+++ portfoliosrv/src/components/About.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import '../styles/About.css';
+import Logo from "../assets/SRVlogo.webp";
+
 
 const About = () => {
   return (
     <section className="about-section">
-      <div className="container">
+      <div className="logo-container">
+      <img src={Logo} alt="Logo" className="logo" />
+    </div>
+      <div className="about-content">
         <h2 className="about-title">About Me</h2>
         <p className="about-text">
-          Hello! My name is Santiago Rambeaud Vazquez, a passionate Full Stack Developer with experience in creating dynamic and innovative web solutions. 
+          Hello! My name is Santiago Rambeaud Vazquez, a passionate Full Stack Developer with experience in creating dynamic and innovative web solutions.
           I am constantly looking for opportunities to challenge myself and grow, combining technical expertise with creative problem-solving.
         </p>
         <p className="about-text">
